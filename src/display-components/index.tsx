49c359ac553bc54247701ae9c5bfaf5dae8b231c
Jameson <jameson@Jamesons-MacBook-Pro.local>
2024-05-23 16:42:50 -0500

    Adding display component and hook init

---
+++ src/display-components/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const DisplayComponents = () => {
+  return (
+    <div>
+      <h1>Display Components</h1>
+    </div>
+  );
+};
+
+export default DisplayComponents;
