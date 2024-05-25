bb635ce7dd79ddd13bf704d3cfdcd43c9e089ca2
Jameson <jameson@Jamesons-MacBook-Pro.local>
2024-05-25 08:57:47 -0500

    Formatting

--- src/display-components/hooks.ts
+++ src/display-components/hooks.ts
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from 'react'
 import * as divAndConq from '../divide-and-conquer'
 
 export const useDisplayCompnentsHooks = () => {
     // configure dropdown for selecting algorithms
     const formattedOptions = Object.keys(divAndConq).map((key) => {
-        return {value: key, label: key}
+        return { value: key, label: key }
     })
 
     return {
-        formattedOptions
+        formattedOptions,
     }
 }
